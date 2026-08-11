3a4dd55832d0f5aa7b4c23d0d2f075c73e32d6bd
Daguilar0123 <aguilard107@gmail.com>
2026-08-11 12:29:42 -0400

    Enhance game card rendering with detailed information and improved structure

--- index.js
+++ index.js
@@ -27,28 +27,36 @@ const gamesContainer = document.getElementById("games-container");
 
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
-
     // loop over each item in the data
-
-
+    for (let i = 0; i < games.length; i++) {
+        const game = games[i];
         // create a new div element, which will become the game card
-
+        const gameCard = document.createElement("div");
 
         // add the class game-card to the list
+        gameCard.classList.add("game-card");
 
-
+        
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        gameCard.innerHTML = `
+            <img src="${game.img}" class="game-img" />
+            <h2>${game.name}</h2>
+            <p>${game.description}</p>
+            <h3>Goal: $${game.goal}</h3>
+            <h3>Pledged: $${game.pledged}</h3>
+            <h3>Backers: ${game.backers}</h3>
+        `;
 
         // append the game to the games-container
-
+        gamesContainer.appendChild(gameCard);
+    }
 }
-
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
+addGamesToPage(GAMES_JSON);
 
 
 /*************************************************************************************
